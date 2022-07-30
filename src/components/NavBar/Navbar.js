9bb3de6dc13dd9007f4d173c90a212a95d2e61b1
mbolonzi-design <mbolonzi.design@gmail.com>
2022-07-30 14:01:48 +0300

    style links

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+import './Navbar.scss'
+
+function NavBar() {
+  return (
+    <div className='navbar'>
+        <div className='navbar-heading'>
+            <h1>Baraka Meals</h1>
+        </div>
+        <div className='navbar-links'>
+            <ul>
+                <Link to ="/"><li>Home</li></Link>
+                <Link to="/categories"><li>Categories</li></Link>
+                <Link to="/random"><li>Random Meal</li></Link>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
