5ce7a517f2e0685e66543f2eb598530c44856031
mbolonzi-design <mbolonzi.design@gmail.com>
2022-07-30 14:13:45 +0300

    add footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Footer.scss'
 
 function Footer() {
   return (
     <div className='footer'>
-        <p>Baraka Meals &copy; 2022</p>
+        <p>Baraka Meals &copy; 2022 Developed <span>Mbolonzi</span> Inc</p>
     </div>
   )
 }
